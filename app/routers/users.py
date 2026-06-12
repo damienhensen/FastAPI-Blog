e8c5f188e4961e6f410ff95952e80073e58e4ce7
Damien <damien.hensen@hva.nl>
2026-06-12 17:24:39 +0200

    feat: protect update and delete user routes

--- app/routers/users.py
+++ app/routers/users.py
@@ -2,13 +2,20 @@ from typing import Annotated
 
 from fastapi import APIRouter, HTTPException, status, Depends
 
+from app.dependencies.auth import get_current_user
 from app.dependencies.users import get_user_service
 from app.dtos.user import UserResponse, UserUpdate
+from app.models.user import User
 from app.services.user_service import UserService
 
 ServiceDep = Annotated[
     UserService,
     Depends(get_user_service),
+]
+
+CurrentUserDep = Annotated[
+    User,
+    Depends(get_current_user),
 ]
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -31,9 +38,11 @@ async def read_user(user_id: int, service: ServiceDep):
     return user
 
 
-@router.patch("/{user_id}", response_model=UserResponse, status_code=status.HTTP_200_OK)
-async def update_user(user_id: int, data: UserUpdate, service: ServiceDep):
-    user = service.update_user(user_id, data)
+@router.patch("/", response_model=UserResponse, status_code=status.HTTP_200_OK)
+async def update_user(
+    data: UserUpdate, current_user: CurrentUserDep, service: ServiceDep
+):
+    user = service.update_user(current_user.id, data)
 
     if user is None:
         raise HTTPException(
@@ -44,9 +53,9 @@ async def update_user(user_id: int, data: UserUpdate, service: ServiceDep):
     return user
 
 
-@router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_user(user_id: int, service: ServiceDep):
-    deleted = service.delete_user(user_id)
+@router.delete("/", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(current_user: CurrentUserDep, service: ServiceDep):
+    deleted = service.delete_user(current_user.id)
 
     if not deleted:
         raise HTTPException(
